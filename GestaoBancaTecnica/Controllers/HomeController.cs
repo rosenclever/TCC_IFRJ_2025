4a74f42cfb2129e2c862d39f4f6d6335aff79e7a
rosenclever <rosenclever@gmail.com>
2025-04-24 16:05:10 -0300

    IACTION e VIEW EXEMPLO

--- GestaoBancaTecnica/Controllers/HomeController.cs
+++ GestaoBancaTecnica/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace GestaoBancaTecnica.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Exemplo()
+        {
+            return View();
+        }
     }
 }
